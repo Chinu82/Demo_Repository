a422fef230ed0e5635b7a5e5ee726b48f1bfd2ad
Chinu82 <114484330+Chinu82@users.noreply.github.com>
2024-03-13 19:52:38 +0530

    Update lol.java

--- lol.java
+++ lol.java
@@ -1,5 +1,5 @@
 class Test {
     public static void main(String[] args) {
-        System.out.println("Hello I am In Github java :");
+        System.out.println("Hello I am In Github  :");
     }
 }
